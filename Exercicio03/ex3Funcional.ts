86206d7c844faac2b87a788bbb3697a720601f54
Lívia Minucelli <liviawrk@gmail.com>
2021-10-29 20:50:55 -0300

    ex3Correcao

--- Exercicio03/ex3Funcional.ts
+++ Exercicio03/ex3Funcional.ts
@@ -2,22 +2,24 @@ const getListFunctional = (list: Array<any>): Array<number> | string => {
     //verificar se todos na lista são numeros.
     const isNumber: boolean = list.every(item => typeof item === 'number');
 
-    if (list.length > 0) {
-        const maxNumber: number = Math.max.apply(Math, list);
-        const minNumber: number = Math.min.apply(Math, list);
+
+    if (list.length > 0 && isNumber === true) {
+        const maxNumber: number = list.reduce((previous: number, current: number) => Math.max(previous, current));
+        const minNumber: number = list.reduce((previous: number, current: number) => Math.min(previous, current));
+
         const meanNumber = (list: Array<number>) => list.reduce((previous: number, current: number) => previous + current) / list.length;
 
         const results: Array<number> = [maxNumber, minNumber, meanNumber(list)];
 
-        return isNumber ? results : "Não existe só numeros na lista";
+        return results;
     } else {
-        return 'lista vazia';
+        return 'Lista vazia ou possui um objeto diferente do tipo numérico.';
     }
 }
 
 //teste
-
 console.log(getListFunctional([99, 2, 13]));
+console.log(getListFunctional([99, 50, 150]));
 console.log(getListFunctional([1, 44, 987, "asdas"]));
 console.log(getListFunctional(["asdas"]));
 console.log(getListFunctional([]));
--- Exercicio03/ex3Imperativo.ts
+++ Exercicio03/ex3Imperativo.ts
@@ -1,31 +1,36 @@
 const getListImperativo = (list: Array<any>): Array<number> | string | boolean => {
     let isNumber: boolean = true;
-    let sum: number = 0;
-    let minNumber: number = list[0];
-    let maxNumber: number = list[0];
-    let meanNumber: number;
 
     for (let i = 0; i < list.length; i++) {
         if (typeof list[i] !== 'number')
             isNumber = false;
     }
 
+    if (isNumber === true && list.length > 0) {
+        let sum: number = 0;
+        let minNumber: number = list[0];
+        let maxNumber: number = list[0];
+        let meanNumber: number;
 
-    for (let i = 0; i < list.length; i++) {
-        sum = sum + list[i];
+        for (let i = 0; i < list.length; i++) {
+            sum = sum + list[i];
 
-        maxNumber = list[i] > maxNumber ? list[i] : maxNumber;
-        minNumber = list[i] < minNumber ? list[i] : minNumber;
+            maxNumber = list[i] > maxNumber ? list[i] : maxNumber;
+            minNumber = list[i] < minNumber ? list[i] : minNumber;
+        }
+        meanNumber = sum / list.length;
+
+
+        const results: Array<number> = [maxNumber, minNumber, meanNumber];
+        return results
+    } else {
+        return "Lista vazia ou possui um objeto diferente do tipo numérico.";
     }
-    meanNumber = sum / list.length;
 
-
-    const results: Array<number> = [maxNumber, minNumber, meanNumber];
-
-    return isNumber === true && list.length > 0 ? results : "Lista vazia ou possui um objeto diferente do tipo numérico.";
 }
 
-console.log(getListImperativo([1, 5, 1231]));
 console.log(getListImperativo([99, 2, 13]));
+console.log(getListImperativo([99, 50, 150]));
 console.log(getListImperativo([1, 5, 1231, "aksjdha"]));
+console.log(getListImperativo(['1, 5, 1231']));
 console.log(getListImperativo([]));
