af70aa0d2d636540169e7bebe2cd73ce1dccd963
Lívia Minucelli <liviawrk@gmail.com>
2021-11-06 08:55:58 -0300

    Delete Exercicio01 directory

--- Exercicio01/A/script.ts
+++
@@ -1,7 +0,0 @@
-function vowelCountByParameter(word: string): number {
-    const wordFilter: number = word.replace(/[^aeiou]/gi, '').length;
-
-    return wordFilter;
-}
-
-console.log("Total de vogais passado via paramêtro: " + vowelCountByParameter("Helpper Origins"));
