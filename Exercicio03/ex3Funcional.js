f496c8da6fe68b2275223ba74f3059964c45fb51
Lívia Minucelli <liviawrk@gmail.com>
2021-11-10 21:39:01 -0300

    ex3upload

---
+++ Exercicio03/ex3Funcional.js
@@ -0,0 +1,19 @@
+var getListFunctional = function (list) {
+    //verificar se todos na lista são numeros.
+    var isNumber = list.every(function (item) { return typeof item === 'number'; });
+    if (list.length > 0 && isNumber === true) {
+        var maxNumber = list.reduce(function (previous, current) { return Math.max(previous, current); });
+        var minNumber = list.reduce(function (previous, current) { return Math.min(previous, current); });
+        var meanNumber = function (list) { return list.reduce(function (previous, current) { return previous + current; }) / list.length; };
+        return [maxNumber, minNumber, meanNumber(list)];
+    }
+    else {
+        return 'Lista vazia ou possui um objeto diferente do tipo numérico.';
+    }
+};
+//teste
+console.log(getListFunctional([99, 2, 13]));
+console.log(getListFunctional([99, 50, 150]));
+console.log(getListFunctional([1, 44, 987, "asdas"]));
+console.log(getListFunctional(["asdas"]));
+console.log(getListFunctional([]));
